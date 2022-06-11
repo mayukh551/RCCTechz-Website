803be35dc60f0a79032deae4e82dcbd5f9d55569
mayukh551 <may14ukh@gmail.com>
2022-06-11 19:56:32 +0530

    teamInfo.json and event layout updated

--- src/Components/Event/EventLayout.jsx
+++ src/Components/Event/EventLayout.jsx
@@ -7,19 +7,13 @@ import {
 
 import "react-vertical-timeline-component/style.min.css";
 
-import classes from "./EventLayout.module.css";
-
 const EventLayout = () => {
     return (
         <div className="py-20">
             <h1 className="text-4xl font-semibold underline underline-offset-8 text-center mb-16">
                 Timeline of Events
             </h1>
-            <VerticalTimeline
-                // className={`${classes["event-content"]}`}
-                layout={"2-columns"}
-                lineColor={"#e63946"}
-            >
+            <VerticalTimeline layout={"2-columns"} lineColor={"#e63946"}>
                 {eventData.map((element, index) => {
                     return (
                         <VerticalTimelineElement
@@ -29,9 +23,14 @@ const EventLayout = () => {
                                 "w-[90%] bp_650:w-[80%] md:w-[70%] bp_1170:w-full"
                             }
                         >
-                            <h3>{element.title}</h3>
-                            <h4>{element.platform}</h4>
+                            <h3 className="text-2xl font-bold mb-3">
+                                {element.title}
+                            </h3>
+                            <h4 className="text-lg font-semibold">
+                                Platform: {element.platform}
+                            </h4>
                             <p>{element.description}</p>
+                            <button className="mt-6 bg-teal-500 text-white py-3 px-6 rounded-lg hover:bg-teal-600">Know More</button>
                         </VerticalTimelineElement>
                     );
                 })}
