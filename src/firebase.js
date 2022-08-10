d8cadba4d58507c005e7c24449d79dde022c4132
Mayukh Bhowmick <82811112+mayukh551@users.noreply.github.com>
2022-08-10 22:10:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,16 +10,6 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBcVb3mw4Gkr2_wenMsocRq0ZL0M127qZc",
-//     authDomain: "rcctechz-website.firebaseapp.com",
-//     projectId: "rcctechz-website",
-//     storageBucket: "rcctechz-website.appspot.com",
-//     messagingSenderId: "757685762067",
-//     appId: "1:757685762067:web:e4a2839744163e427ea7e6",
-//     measurementId: "G-Y13M7J74N2"
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
