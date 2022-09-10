807cce3bf727be0b00f68b3961f9dc02b39b2848
mayukh551 <may14ukh@gmail.com>
2022-09-10 17:49:02 +0530

    Footer Links Updated

--- src/Components/Home/HFooter.jsx
+++ src/Components/Home/HFooter.jsx
@@ -144,7 +144,7 @@ const HFooter = () => {
                             </svg>
                         </a>
                         <a
-                            href="https://www.linkedin.com/company/rcctechz/mycompany/"
+                            href="https://www.linkedin.com/company/74085329/admin/"
                             rel="noreferrer"
                             target="_blank"
                             className="ml-3 text-gray-400"
@@ -178,44 +178,3 @@ const HFooter = () => {
 };
 
 export default HFooter;
-
-// eslint-disable-next-line no-lone-blocks
-{
-    /* <div className="py-24 px-12 bg-black text-white flex flex-row justify-center space-x-48">
-    <div>
-        <h4 className="font-bold">Title Here</h4>
-        <p className="w-44 mt-7">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Commodi, veritatis.
-        </p>
-    </div>
-    <div>
-        <h4 className="font-bold">About</h4>
-        <ul className="mt-7 space-y-3">
-            <li>History</li>
-            <li>Our Team</li>
-            <li>Brand Guidelines</li>
-            <li>Terms & Condition</li>
-            <li>Privacy Policy</li>
-        </ul>
-    </div>
-    <div>
-        <h4 className="font-bold">Services</h4>
-        <ul className="mt-7 space-y-3">
-            <li>How to Order</li>
-            <li>Our Product</li>
-            <li>Order Status</li>
-            <li>Promo</li>
-            <li>Payment Method</li>
-        </ul>
-    </div>
-    <div>
-    <h4 className="font-bold">Other</h4>
-        <ul className="mt-7 space-y-3">
-            <li>Contact Us</li>
-            <li>Help</li>
-            <li>Privacy</li>
-            </ul>
-    </div>
-</div> */
-}
